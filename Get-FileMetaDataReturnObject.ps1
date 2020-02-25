0331ed5e20914bc22676b578829108f82bc2a434
Mathias Essenpreis <esse@gmx.de>
2020-02-25 13:46:56 -0300

    'TODO | Where-Object { $_.Rating -like '4' }

---
+++ Get-FileMetaDataReturnObject.ps1
@@ -0,0 +1,16 @@
+﻿# -----------------------------------------------------------------------------
+# Script: Get-FileMetaDataReturnObject.ps1
+# Author: ed wilson, msft
+# Date: 01/24/2014 12:30:18
+# Keywords: Metadata, Storage, Files
+# comments: Uses the Shell.APplication object to get file metadata
+# Gets all the metadata and returns a custom PSObject
+# it is a bit slow right now, because I need to check all 266 fields
+# for each file, and then create a custom object and emit it.
+# If used, use a variable to store the returned objects before attempting
+# to do any sorting, filtering, and formatting of the output.
+# To do a recursive lookup of all metadata on all files, use this type
+# of syntax to call the function:
+# Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName
+# note: this MUST point to a folder, and not to a file.
+# -----------------------------------------------------------------------------
